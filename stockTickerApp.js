951951c8b1719d1cb86997268f3d71a3bf9f2d06
jdeluca02 <72902005+jdeluca02@users.noreply.github.com>
2021-04-28 20:32:34 -0400

    Add files via upload

--- stockTickerApp.js
+++ stockTickerApp.js
@@ -1,146 +1,233 @@
+//var http = require('http');
+//var url = require('url');
+//var path = require('path');
+//var fs = require('fs');
+//var qs = require('querystring');
+//const MongoClient = require('mongodb').MongoClient;
+//
+//// connection string
+//const mongoUrl = "mongodb+srv://jdeluc02:Tufts2021@cluster.g81rs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//
+//
+//const server = http.createServer((req, res) => {
+//    // create filepath for any page
+//    var filePath = path.join(
+//        __dirname,
+//        'public',
+//        req.url === '/' ? 'index.html' : req.url
+//    );
+//    // console.log(filePath);
+//
+//    // Ensure correct content type is picked
+//    var contentType = getContType(filePath);
+//    // console.log(contentType);
+//
+//    if (req.url == '/result') {
+//        res.writeHead(200, {'Content-Type': 'text/html'}); 
+//        // res.write ("Process the POST request<br>"); 
+//        pdata = ""; 
+//        req.on('data', data => {
+//            pdata += data.toString();
+//            // res.write(pdata);
+//        })
+//        .on('end', () => {
+//            pdata = qs.parse(pdata);
+//
+//            var type = pdata['input_type'];
+//            var target = pdata['user_input'];
+//            console.log(`User put in ${target} for ${type}.`);
+//
+//            connectAndDisplay(target, type, res);
+//        });
+//    } 
+//    else { 
+//        fs.readFile(filePath, function(err, content) {
+//            if (err) { 
+//                display404Page(err, res);
+//            }
+//            else { displayCurrentContent(content, contentType, res); }
+//        });
+//    }
+//});
+//
+//// the port in a variable using environment variable;
+//const port = process.env.PORT || 8080;
+//server.listen(port, () => console.log(`Server running on port ${port}`));
+//
+///* getContType
+// * Returns the string for Content-Type given a files path.
+// * Limited to the cases shown below. Default will be html.
+// */
+//function getContType(filePath) {
+//    var ext = path.extname(filePath);
+//    switch(ext) {
+//        case '.html':
+//            return 'text/html';
+//        case '.js':
+//            return 'text/javascript';
+//        case '.css':
+//            return 'text/css';
+//        default:
+//            return 'text/html';
+//    }
+//}
+//
+///* display404Page
+// * displays error page when user attempts to view non page on server
+// */
+//function display404Page(err, res) {
+//    if (err.code == 'ENOENT') { 
+//        // Display 404 page
+//        fs.readFile(path.join(__dirname, 'public', '404.html'), 
+//                (err, content) => {
+//                    res.writeHead(200, { 'Content-Type': 'text/html' });
+//                    res.end(content, 'utf8');
+//                });
+//    }
+//}
+//
+///* displayCurrentContent
+// * Takes the response from server and displays content.
+// */
+//function displayCurrentContent(content, contentType, res) {
+//    res.writeHead(200, { 'Content-Type': contentType });
+//    res.end(content, 'utf8');
+//}
+//
+///* connectAndDisplay
+// * Query the database and display information requested
+// */
+//async function connectAndDisplay(target, type, res) {
+//    var t = "";
+//
+//    MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, async (err, database) => {
+//        if (err) {
+//            console.log("Connection to Mongo err: " + err);
+//            return;
+//        }
+//
+//        // get database and collection object
+//        var dbo = database.db("StockDB");
+//        var collection = dbo.collection('companies');
+//
+//        try {
+//            theQuery = "";
+//            queryOptions = "";
+//            if (type == "company") {
+//                theQuery = {name: target};
+//                queryOptions = {sort:{name:1}, projection:{_id:0, name:1, ticker:1}};
+//                t += `<h2>Company: ${target} has ticker: </h2><br>`;
+//            } else if (type == "ticker") {
+//                theQuery = {ticker: target};
+//                queryOptions = {sort:{name:1}, projection:{_id:0, name:1, ticker:1}};
+//                t += `<h2>Companies with ticker code ${target} are: </h2><br>`;
+//            }
+//
+//            var result = await collection.find(theQuery, queryOptions).toArray();
+//            // console.log(result);
+//
+//            if (result.length === 0) {
+//                console.log(`No results found`);
+//                t += `No results found.`;
+//            } else {
+//                result.forEach(function (curr) {
+//                    console.log(`${curr.name} has ticker ${curr.ticker}`);
+//                    t += `${curr.name} (${curr.ticker})<br>`;
+//                });
+//            }
+//        }
+//
+//        finally {
+//            // console.log(`Writing Result and Closing DB`);
+//            res.end(t);
+//            database.close();
+//        }
+//    });
+//}
+
+  
 var http = require('http');
-var url = require('url');
-var path = require('path');
+var port = process.env.PORT || 3000;
 var fs = require('fs');
 var qs = require('querystring');
-const MongoClient = require('mongodb').MongoClient;
 
-// connection string
-const mongoUrl = "mongodb+srv://jdeluc02:Tufts2021@cluster.g81rs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+http.createServer(function (req,res)
+{	
+	//Load home page
+	 if (req.url === "/") {  
+	 	file = 'index.html';  
+	 	fs.readFile(file, function(err, txt) {  
+	 		res.writeHead(200, {'Content-Type': 'text/html'});           
+	 		res.write(txt);          
+	 		res.end();  
+	 	});  
+	 }
+
+	//Get form data
+	else if (req.url === "/process") {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		var pdata = "";
+		req.on('data', data => {
+			pdata += data.toString();
+		});
+		
+		req.on('end', () => {
+			pdata = qs.parse(pdata);
+			search = pdata['search'];
+			type = pdata['c_or_t'];
 
 
-const server = http.createServer((req, res) => {
-    // create filepath for any page
-    var filePath = path.join(
-        __dirname,
-        'public',
-        req.url === '/' ? 'index.html' : req.url
-    );
-    // console.log(filePath);
+			res.write("You are searching for " + search + "<br><br>");
+			res.write("Your results are: " + "<br>");
 
-    // Ensure correct content type is picked
-    var contentType = getContType(filePath);
-    // console.log(contentType);
+			
+			
+			//Connect to database
+			var mongo = require('mongodb');
+			var MongoClient = mongo.MongoClient;
+			const url = "mongodb+srv://jdeluc02:Tufts2021@cluster.g81rs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-    if (req.url == '/result') {
-        res.writeHead(200, {'Content-Type': 'text/html'}); 
-        // res.write ("Process the POST request<br>"); 
-        pdata = ""; 
-        req.on('data', data => {
-            pdata += data.toString();
-            // res.write(pdata);
-        })
-        .on('end', () => {
-            pdata = qs.parse(pdata);
-
-            var type = pdata['input_type'];
-            var target = pdata['user_input'];
-            console.log(`User put in ${target} for ${type}.`);
-
-            connectAndDisplay(target, type, res);
-        });
-    } 
-    else { 
-        fs.readFile(filePath, function(err, content) {
-            if (err) { 
-                display404Page(err, res);
-            }
-            else { displayCurrentContent(content, contentType, res); }
-        });
-    }
-});
-
-// the port in a variable using environment variable;
-const port = process.env.PORT || 8080;
-server.listen(port, () => console.log(`Server running on port ${port}`));
-
-/* getContType
- * Returns the string for Content-Type given a files path.
- * Limited to the cases shown below. Default will be html.
- */
-function getContType(filePath) {
-    var ext = path.extname(filePath);
-    switch(ext) {
-        case '.html':
-            return 'text/html';
-        case '.js':
-            return 'text/javascript';
-        case '.css':
-            return 'text/css';
-        default:
-            return 'text/html';
-    }
-}
-
-/* display404Page
- * displays error page when user attempts to view non page on server
- */
-function display404Page(err, res) {
-    if (err.code == 'ENOENT') { 
-        // Display 404 page
-        fs.readFile(path.join(__dirname, 'public', '404.html'), 
-                (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf8');
-                });
-    }
-}
-
-/* displayCurrentContent
- * Takes the response from server and displays content.
- */
-function displayCurrentContent(content, contentType, res) {
-    res.writeHead(200, { 'Content-Type': contentType });
-    res.end(content, 'utf8');
-}
-
-/* connectAndDisplay
- * Query the database and display information requested
- */
-async function connectAndDisplay(target, type, res) {
-    var t = "";
-
-    MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, async (err, database) => {
-        if (err) {
-            console.log("Connection to Mongo err: " + err);
-            return;
-        }
-
-        // get database and collection object
-        var dbo = database.db("StockDB");
-        var collection = dbo.collection('companies');
-
-        try {
-            theQuery = "";
-            queryOptions = "";
-            if (type == "company") {
-                theQuery = {name: target};
-                queryOptions = {sort:{name:1}, projection:{_id:0, name:1, ticker:1}};
-                t += `<h2>Company: ${target} has ticker: </h2><br>`;
-            } else if (type == "ticker") {
-                theQuery = {ticker: target};
-                queryOptions = {sort:{name:1}, projection:{_id:0, name:1, ticker:1}};
-                t += `<h2>Companies with ticker code ${target} are: </h2><br>`;
-            }
-
-            var result = await collection.find(theQuery, queryOptions).toArray();
-            // console.log(result);
-
-            if (result.length === 0) {
-                console.log(`No results found`);
-                t += `No results found.`;
-            } else {
-                result.forEach(function (curr) {
-                    console.log(`${curr.name} has ticker ${curr.ticker}`);
-                    t += `${curr.name} (${curr.ticker})<br>`;
-                });
-            }
-        }
-
-        finally {
-            // console.log(`Writing Result and Closing DB`);
-            res.end(t);
-            database.close();
-        }
-    });
-}
+			MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {    
+				if(err) { 
+					console.log("Connection err: " + err); 
+					return; 
+				}
+			
+ 
+				var dbo = db.db("companies");    
+				var collection = dbo.collection("companies");   
+			
+			    //Call the query
+			    if (type == "Company") {
+			    	theQuery = {Company: search};
+			    }
+			    if (type == "Ticker") {
+			    	theQuery = {Ticker: search};
+			    }
+			    
+				collection.find(theQuery).toArray(function(err, items) {  
+					if (err) {
+						console.log("Error: " + err);  
+					} 
+					else {
+						if (items.length == 0){
+							res.write("None found! Either check your search for spelling mistakes, or the search you're looking for doesn't exist!");
+						}
+						else {
+							for (i=0; i<items.length; i++) {
+								res.write(items[i].Company + " has ticker " + items[i].Ticker + "<br>");  
+							}
+						}
+						res.end();
+					}     
+					db.close();
+		        });
+	        });
+		});
+	}
+	else{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write ("Unknown page request");
+		res.end();  
+	}
+}).listen(port);
